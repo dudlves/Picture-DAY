ea18758eccd5c8fa23ef6a43009208dbd3d83c24
dudlves <dudlves17@gmail.com>
2022-11-30 21:38:16 -0300

    adicionando função openTexto

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-const button = document.querySelector("button")
-const texto = document.getElementById("container")
 
 
-button.addEventListener('click', openTexto);
-
-function openTexto(evt){
-    if(evt){ 
-        texto.style.display = "block"
-    }else{
-        texto.style.display = "none";
-    }
+function openTexto(evt) {
+    var display = document.getElementById(evt).style.display;
+    if(display == "none")
+        document.getElementById(evt).style.display = 'block';
+    else
+        document.getElementById(evt).style.display = 'none';
 }
-console.log(texto)
