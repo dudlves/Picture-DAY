57b7b4809f9dc5a134913052afdbe9513ad02692
dudlves <dudlves17@gmail.com>
2022-12-02 12:12:52 -0300

    tirando o onclick do HTML

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 
+var btn = document.getElementById('texto');
+var container = document.getElementById('container');
+btn.addEventListener('click', function() {
+  if(container.style.display === 'block') {
+      container.style.display = 'none';
+  } else {
+      container.style.display = 'block';
+  }
+})
 
-function openTexto(evt) {
-    var display = document.getElementById(evt).style.display;
-    if(display == "none")
-        document.getElementById(evt).style.display = 'block';
-    else
-        document.getElementById(evt).style.display = 'none';
-}
+// document.querySelector("input").addEventListener("click", async () => {
+//     const data = await fetch('https://api.nasa.gov/planetary/apod? api_key=Xal2ri16laPoYH9tOv61Slq1ZtcWCERvZEyB6hZM')
+//     const result = await data.json()
+//     console.log(result)
+// })
